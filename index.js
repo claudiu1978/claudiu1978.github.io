faf15d8f7c2e249c78f27b2584458116c80ab663
Claudiu Cimpean <cimpean1@gmail.com>
2022-07-04 21:02:02 +0300

    firs version of cv

---
+++ index.js
@@ -0,0 +1,18 @@
+console.info(2);
+console.debug(2 + 3);
+console.warn(6 * 3 - 10);
+console.error("started");
+var myName = "Claudiu";
+var age = "42";
+
+// console.info("My name is " + myName + " I'am " + age + "years old");
+age = 30;
+// console.info("My name is " + myName + " I'am " + age + "years old");
+age = 31;
+// console.info("My name is " + myName + " I'am " + age + "years old");
+var jobTitle = "Cursant web developer";
+var mottoElement = document.getElementById("motto");
+console.info(mottoElement);
+console.warn(mottoElement.innerHTML);
+// mottoElement.innerHTML = mottoElement.innerHTML + "&" + jobTitle;
+mottoElement.innerHTML += "&" + jobTitle;
