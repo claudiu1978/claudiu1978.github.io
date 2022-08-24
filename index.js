b9c8da5bf39ffa8cc1968898df6a7e70f30fd82b
Claudiu Cimpean <cimpean1@gmail.com>
2022-08-24 10:58:31 +0300

    loadskills

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ console.info(2);
 console.debug(2 + 3);
 console.warn(6 * 3 - 10);
 // console.error("started");
-var myName = "Claudiu";
-var age = "30";
+// var myName = "Claudiu";
+// var age = "30";
 
 // console.info("My name is " + myName + " I'am " + age + "years old");
-age = 30;
+// age = 30;
 // console.info("My name is " + myName + " I'am " + age + "years old");
-age = 31;
-console.info("My name is " + myName + " I'am " + age + "years old");
+// age = 31;
+// console.info("My name is " + myName + " I'am " + age + "years old");
 var jobTitle = " Cursant web developer";
 var mottoElement = document.getElementById("motto");
 console.info(mottoElement);
@@ -86,9 +86,9 @@ function loadSkills() {
     .then(function (response) {
       return response.json();
     })
-    .then(function (skills) {
-      console.warn("am primit ceva sau nu", skills);
-      displaySkills(skills);
+    .then(function (serverSkills) {
+      console.warn(serverSkills);
+      1 + displaySkills(serverSkills);
     });
 }
 loadSkills();
